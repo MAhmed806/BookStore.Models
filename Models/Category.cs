c188eac056fc70d74831668aa527d293c04588b1
Ahmed <Ahmed@DESKTOP-N2130HK>
2023-08-22 19:23:10 +0500

    Initial Commit

---
+++ Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+
+        [StringLength(20)]
+        public string Name { get; set; }
+        [Required]
+        [Range(0, 100)]
+        [DisplayName("Display Order")]
+        public int? DisplayOrder { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace BookStore.Models.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        [Display(Name = "List Price")]
+        [Range(1, 1000)]
+        public double ListPrice { get; set; }
+        [Required]
+        [Display(Name = "Price of 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+        [Required]
+        [Display(Name = "Price of 50+")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [Display(Name = "Price of 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+
+        [DisplayName("Category")]
+        [Required(ErrorMessage = "Category Field is Required")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+        [ValidateNever]
+        [DisplayName("Image")]
+        public string ImageUrl { get; set; }
+    }
+}
