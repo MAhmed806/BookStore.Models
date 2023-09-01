4fec26db997a117131dde3a88a10fb586ab0d3bb
Ahmed <Ahmed@DESKTOP-N2130HK>
2023-09-02 02:21:03 +0500

    Email Sender Configuration

---
+++ Models/MailSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Models
+{
+    public class MailSettings
+    {
+            public string Username { get; set; }
+            public string DisplayName { get; set; }
+            public string Password { get; set; }
+            public string Host { get; set; }
+            public int Port { get; set; }
+        public string Security { get; set; }
+     
+    }
+}
